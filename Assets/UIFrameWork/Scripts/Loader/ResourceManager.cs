6140a5a00e436b5c0387e57cdd82b8a8138cb3c2
chiguozi <qdlzr456@163.com>
2017-04-28 13:32:50 +0800

    first

---
+++ Assets/UIFrameWork/Scripts/Loader/ResourceManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class ResourceManager
+{
+
+    //@todo  assetbundle支持和异步加载支持
+    public static void LoadAsset(string abName, Action<object> callback, string assetName = "")
+    {
+        LoadAssetInternal(abName, callback, assetName);
+    }
+
+
+
+    static void LoadAssetInternal(string path, Action<object> callback, string assetName)
+    {
+        var obj = Resources.Load(path);
+        if (null != callback)
+            callback(obj);
+    }
+
+}
---
+++ Assets/UIFrameWork/Scripts/UIBaseView.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class UIBaseView : MonoBehaviour
+{
+    protected enum LoadingState
+    {
+        None = 0,
+        Loading = 1,
+        Finish = 2,
+        Failed = 3,
+    }
+
+    protected string uiName = "";
+    protected string uiPath = "";
+    public string UIName { get { return uiName; } }
+
+    protected bool _isActive = false;
+    public virtual bool isActive
+    {
+        get { return _isActive; }
+        set
+        {
+            //如果UI需要强制刷新active  可以将这个判断去掉
+            if (_isActive == value)
+                return;
+            _isActive = value;
+            if (uiGameObject != null)
+                uiGameObject.SetActive(_isActive);
+        }
+    }
+
+    protected LoadingState loadingState = LoadingState.None;
+    public bool isLoadComplete { get { return loadingState == LoadingState.Loading; } }
+
+    public GameObject uiGameObject;
+    public Transform uiTransform;
+    public Bounds uiBounds = new Bounds(Vector3.zero, Vector3.zero);
+    protected Vector3 localPos = Vector3.zero;
+
+    protected virtual void Awake()
+    {
+        uiName = this.GetType().Name; 
+    }
+
+    //参数和接口由UI各自提供，基类不提供通用参数处理
+    public virtual void OnShow()
+    {
+        if (loadingState == LoadingState.Loading || loadingState == LoadingState.Finish)
+            return;
+        loadingState = LoadingState.Loading;
+
+        isActive = true;
+
+        ResourceManager.LoadAsset(uiPath + uiName, LoadComplete);
+    }
+
+
+    protected virtual void LoadComplete(object obj)
+    {
+    }
+
+}
+
